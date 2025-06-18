77c424cbdaa7d7ca737a663e96bf0857ef6cc38f
ocai0 <caioalvesfernandes@gmail.com>
2025-06-17 21:28:18 -0300

    Criado pagina inicial (/)

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -9,8 +9,14 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div className="container-fluid">
-      
+    <div className="container-fluid d-flex vw-10 vh-100 bg-dark text-light align-items-end p-4">
+      <div className="col-12 col-sm-8">
+        <h1 className="display-3 mb-5">Bem vindo a nossa página inicial</h1>
+        <div className="d-flex gap-3 mb-5">
+          <a href="#" className="btn btn-lg btn-primary">Cadastre-se</a>
+          <a href="#" className="btn btn-lg btn-outline-primary">Login</a>
+        </div>
+      </div>
     </div>
   )
 }
