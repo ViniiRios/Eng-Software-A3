08376ed32ede2245fe8cc2d867ff237b6d9c3168
ocai0 <caioalvesfernandes@gmail.com>
2025-06-20 15:09:13 -0300

    Atualizado codigo do Users para utilizar corretamente o Database e começar a retornar os valores necessários

--- server/src/api/User.js
+++ server/src/api/User.js
@@ -1,13 +1,49 @@
 class Users {
-    constructor() {}
+    constructor(database) {
+        this.db = database;
+    }
 
-    static findAll(filterOptions) {
-        return {
-            result: true
+    async findAll(filterOptions) {
+        let query = `SELECT name, email from users`;
+        let limit = 1000
+        let offset = 0
+        Object.keys(filterOptions).forEach(filterKey => {
+            switch(filterKey.toLowerCase()) {
+                case "search_for": {
+                    query += " ";
+                    query += `WHERE name LIKE "%${filterOptions[filterKey]}%"`;
+                    break;
+                }
+                case "page": {
+                    const value = parseInt(filterOptions[filterKey]);
+                    if(isNaN(value) === false) {
+                        offset = value;
+                    }
+                    break;
+                }
+                case "page_size": {
+                    const value = parseInt(filterOptions[filterKey])
+                    if(isNaN(value) === false) {
+                        limit = value;
+                    }
+                    break;
+                }
+            }
+        })
+        query += " ";
+        query += `LIMIT ${limit} OFFSET ${offset}`
+        console.log(`Users::FindAll`, {query})
+        try {
+            const result = await this.db.all(query);
+            return result;
+        }
+        catch(e) {
+            console.error(e)
+            return false;
         }
     }
 
-    static find(filterOptions) {
+    find(filterOptions) {
         const {nickname} = filterOptions
         return {
             result: true
