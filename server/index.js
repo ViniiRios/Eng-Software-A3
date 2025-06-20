a4667f78b6b57ff59cc87b91a7737718508909a2
ocai0 <caioalvesfernandes@gmail.com>
2025-06-20 15:23:03 -0300

    Atualizado codigo do endpoiont '/users'

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
-import express from "express"
+import express from 'express'
 import Database from './src/Database.js'
 import Users from './src/api/User.js'
 
 const app = express()
 const port = 3000
+const databaseAPI = new Database();
+const userCrud = new Users(databaseAPI);
 
 app.get('/tasks/:id', (req, res) => {
     res.status(200).send(true)
 })
 
-app.get("/users", (req, res) => {
-    const result = Users.findAll();
+app.get("/users", async (req, res) => {
+    const result = await userCrud.findAll(req.query);
     res.status(200).json(result)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
