6a07e67b9c823661a7a108f4203c0a240e4b41e5
ocai0 <caioalvesfernandes@gmail.com>
2025-06-21 07:59:07 -0300

    Adicionado codigo para simular multiplos pipes

--- client/app/routes/kanban.tsx
+++ client/app/routes/kanban.tsx
@@ -50,17 +50,35 @@ export default async function Kanban() {
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nemo, amet inventore modi iusto voluptatem ratione, quam vel corporis qui impedit, ipsa sapiente necessitatibus sit? Obcaecati explicabo reprehenderit est nihil."
         }
     ]
+    let pipes = [
+        {
+            title: "Teste",
+            cards
+        },
+        {
+            title: "Teste",
+            cards
+        },
+        {
+            title: "Teste",
+            cards
+        },
+    ]
     return (
-        <div className="container-fluid d-flex vw-10 vh-100 bg-light align-items-end p-4 overflow-x-visible gap-3">
-                <div className="bg-light p-3 rounded d-flex flex-column h-100" style={pipeStyling}>
-                    <h2 className="h5 m-0">Teste</h2>
-                    <hr className="mt-2 mb-4 mx-0 border-primary border-bottom border-2"/>
-                    <div className="d-flex flex-column overflow-y-auto flex-grow-1 gap-3">
-                        {
-                            cards.map((card, index) => <Card key={index} title={card.title} description={card.description} />)
-                        }
-                    </div>
-                </div>
+        <div className="container-fluid d-flex vw-10 vh-100 bg-light align-items-end p-4 pb-0 overflow-x-auto overflow-y-hidden gap-3">
+                {
+                    pipes.map(({title, cards}) => 
+                        <div className="bg-light p-3 pb-0 rounded d-flex flex-column h-100 flex-shrink-0" style={pipeStyling}>
+                            <h2 className="h5 m-0">{title}</h2>
+                            <hr className="mt-2 mb-4 mx-0 border-primary border-bottom border-2"/>
+                            <div className="d-flex flex-column overflow-y-auto flex-grow-1 gap-3">
+                                {
+                                    cards.map((card, index) => <Card key={index} title={card.title} description={card.description} />)
+                                }
+                            </div>
+                        </div>
+                    )
+                }
         </div>
     )
 }
