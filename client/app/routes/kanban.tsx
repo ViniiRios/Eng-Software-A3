5892351683629776e1e0cb81415e0bef0679c0af
ocai0 <caioalvesfernandes@gmail.com>
2025-06-21 16:49:49 -0300

    Atualizado o codigo para que ele acesse o endpoint (WIP)

--- client/app/routes/kanban.tsx
+++ client/app/routes/kanban.tsx
@@ -1,21 +1,44 @@
+import { useEffect } from "react";
 import Card from "~/components/Card";
 
-async function getPipeInformation() {
+async function getPipeInformation(): Promise<{id: number, title: string}[]> {
     const request = await fetch(`http://localhost:3000/pipes`)
     const response = request.json();
     return response
 }
 
-async function getAllCardsOrderedByPipe() {
+async function getAllTasksForPipe(pipeId: number) {
     const request = await fetch(`http://localhost:3000/tasks`)
-    let response = await request.json();
-    return response
+    let response = await request.json() as {title: string, description: string, id: number}[];
+    return {pipeId, response}
 }
 
 const pipeStyling = {
     width: "400px"
 }
-export default async function Kanban() {
+export default function Kanban() {
+    /** Call callback function on component load, and only once because of the [""] value */
+    useEffect(() => {
+        getPipeInformation()
+        .then(pipes => {
+            console.log({pipes})
+            const _pipesMap = new Map()
+            pipes.forEach(pipe => _pipesMap.set(pipe.id, pipe))
+            const requests = pipes.map(pipe => getAllTasksForPipe(pipe.id));
+            Promise
+            .all(requests)
+            .then(tasks => {
+                console.log({tasks})
+                pipes = tasks.map(task => {
+                    return {
+                        title: _pipesMap.get(task.pipeId),
+                        id: task.pipeId,
+                        cards: task.response,
+                    }
+                })
+            })
+        })
+    }, [""])
     let cards = [
         {
             title: "title",
@@ -67,13 +90,13 @@ export default async function Kanban() {
     return (
         <div className="container-fluid d-flex vw-10 vh-100 bg-light align-items-end p-4 pb-0 overflow-x-auto overflow-y-hidden gap-3">
                 {
-                    pipes.map(({title, cards}) => 
-                        <div className="bg-light p-3 pb-0 rounded d-flex flex-column h-100 flex-shrink-0" style={pipeStyling}>
+                    pipes.map(({title, cards}, pipeIndex) => 
+                        <div key={pipeIndex} className="bg-light p-3 pb-0 rounded d-flex flex-column h-100 flex-shrink-0" style={pipeStyling}>
                             <h2 className="h5 m-0">{title}</h2>
                             <hr className="mt-2 mb-4 mx-0 border-primary border-bottom border-2"/>
                             <div className="d-flex flex-column overflow-y-auto flex-grow-1 gap-3">
                                 {
-                                    cards.map((card, index) => <Card key={index} title={card.title} description={card.description} />)
+                                    cards.map((card, cardIndex) => <Card key={cardIndex} title={card.title} description={card.description} />)
                                 }
                             </div>
                         </div>
