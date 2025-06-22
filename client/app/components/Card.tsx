a2f7531e5b679d8fdba02395b335feb541c12468
ocai0 <caioalvesfernandes@gmail.com>
2025-06-22 12:48:55 -0300

    Aplicado estilo novo nos cards do kanban

--- client/app/components/Card.tsx
+++ client/app/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({title, description} = {title: "", description: ""}) {
     return (
-        <div className="p-3 shadow-sm rounded-3 w-100 border">
+        <div className="task w-100">
             <h3 className="fw-semibold fs-6">{title}</h3>
             <p className="small mb-2">{description}</p>
         </div>
